1d7b72ad9f06206f64b2117b6f303d06fbc31ca2
arleyyap <arleyyap@hotmail.com>
2019-04-17 18:09:34 -0500

    Creacion de cuenta , registro por correo y contraseña queda pendiente nombre, apellido , tipo etc

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private authService:AuthService) { }
+  public email:string = '';
+  public password: string = '';
 
   ngOnInit() {
+  }
+
+  onAddUser(){
+    console.log('email',this.email);
+    console.log('password',this.password);
+    this.authService.registerUser(this.email,this.password)
+    .then((res)=>{
+      this.router.navigate(['users/prueba']);
+    }).catch(  err =>console.log('err', err.message));
   }
 
 }
