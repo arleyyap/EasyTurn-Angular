9a38a9c821b878a411ca045a1d5e719bbcf85697
arleyyap <arleyyap@hotmail.com>
2019-05-03 16:54:31 -0500

    Validacion de Tipo de Usuario Login (Restaurante, administrativo)

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -4,6 +4,9 @@ import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
 import { AuthService } from './../../../services/auth.service';
 import { DataApiService } from './../../../services/data-api.service';
+import { Usuarios } from './../../../domain/usuarios';
+import { map } from 'rxjs/operators';
+import { stringify } from 'querystring';
 
 
 @Component({
@@ -21,7 +24,9 @@ export class LoginComponent implements OnInit {
               private authService: AuthService, private dataApiService: DataApiService) {}
   public email: string;
   public password: string;
+  public usuarios: Usuarios;
   usuario = []; // Se declara el arreglo para tener la informacion del usuario
+
 
   ngOnInit() {
   }
@@ -30,14 +35,35 @@ export class LoginComponent implements OnInit {
     this.usuario = [];
     this.authService.loginEmailUser(this.email, this.password)
       .then((res) => {
-        this.dataApiService.searchUserForEmail(this.email).subscribe((datausuarios)=>{
-          datausuarios.forEach((usuario: any) => {
-            this.usuario.push({
-              id: usuario.payload.doc.id,
-              data: usuario.payload.doc.data()
-            });
+        this.dataApiService.searchUserForEmail(this.email)
+        .pipe(map(docArray =>{
+          return docArray.map(doc =>{
+            console.log(doc);
+            console.log(doc.payload.doc.data()['nombre']);
+            const data = {
+              id: doc.payload.doc.id,
+              nombre: doc.payload.doc.data()['nombre'],
+              apellido: doc.payload.doc.data()['apellido'],
+              telefono: doc.payload.doc.data()['telefono'],
+              email: doc.payload.doc.data()['email'],
+              contrasena: doc.payload.doc.data()['contraseña'],
+              tipoUsuario: doc.payload.doc.data()['tipoUsuario']
+            }
+            if (data.tipoUsuario == 1) {
+              console.log('Bienvenido Tipo de Usuario Restaurante');
+              this.router.navigate(['user/restaurante/restaurante']);
+            }else if (data.tipoUsuario == 2){
+              console.log('Bienvenido Tipo de Usuario Administrativo');
+              this.router.navigate(['user/administrativo']);
+            }else{
+              console.log('TU NO PUEDES ENTRAR');
+            }
           });
+        }))
+        .subscribe(exercices =>{
+
         });
+
         console.log(Object.keys(this.usuario));
         //this.onLoginRedirect();
       }).catch(err => console.log('err', err.message));
